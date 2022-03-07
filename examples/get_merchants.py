cf070e302097545234955bb976ca9c8646598712
LJsson <ludvig.johansson@live.se>
2022-03-07 16:45:18 +0100

    payment endpoints funkar

--- examples/get_merchants.py
+++ examples/get_merchants.py
@@ -3,6 +3,6 @@ from ping.tenant import Tenant
 tenant_id = "a2a4f648-a50b-42fb-bda8-00c6e2f295ea"
 tenant = Tenant(tenant_id= tenant_id)
 
-list_of_merchants = tenant.get_merchants()
+list_of_merchants = tenant.Merchant().get_merchants()
 print(list_of_merchants.text)
 print(list_of_merchants.status_code)
--- ping/tenant.py
+++ ping/tenant.py
@@ -16,18 +16,22 @@ class Tenant():
             "Accept": "application/json",
             "tenant_id": tenant_id
         }
+    
   
+#Merchant endpoints
   def get_merchants(self):
     return merchants.get_merchants(self.headers, self.base_url)
-  
   def create_new_merchant(self, obj):
     return merchants.create_new_merchant(self.headers, self.base_url, obj)
-  
   def get_specific_merchant(self,  merchant_id):
     return merchants.get_specific_merchant(self.headers, self.base_url, merchant_id)
 
+#Payment_order endpoints
   def payments_orders(self):
     return paymentOrders.PaymentOrders(self.headers, self.base_url)
   
-  def payments(self):
-    return payments.Payments(self.headers, self.base_url)
+#Payment endpoints
+  def initiate_payment(self, obj, payment_order_id):
+    return payments.initiate_payment(self.headers, self.base_url, obj, payment_order_id)
+  def get_payment(self, payment_order_id, payment_id):
+    return payments.get_payment(self.headers, self.base_url, payment_order_id, payment_id)
