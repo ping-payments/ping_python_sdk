4a1e57fc94015ec2efc6436ab669ccc9052d100c
LJsson <ludvig.johansson@live.se>
2022-03-08 15:59:37 +0100

    global test

--- examples/get_merchants.py
+++ examples/get_merchants.py
@@ -1,8 +1,8 @@
 from ping.tenant import Tenant
 
 tenant_id = "a2a4f648-a50b-42fb-bda8-00c6e2f295ea"
-tenant = Tenant(tenant_id= tenant_id)
+tenant = Tenant(tenant_id = tenant_id)
 
-list_of_merchants = tenant.Merchant().get_merchants()
+list_of_merchants = tenant.Merchant.get_merchants()
 print(list_of_merchants.text)
 print(list_of_merchants.status_code)
