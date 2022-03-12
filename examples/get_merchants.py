bf5b859d9054d431837178161f21373fa128415c
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-03-12 01:03:32 +0100

    refactoring

--- examples/get_merchants.py
+++ examples/get_merchants.py
@@ -1,8 +1,9 @@
 from ping.payments_api import PaymentsApi
 
-pay_api = PaymentsApi("a2a4f648-a50b-42fb-bda8-00c6e2f295ea")
+payments_api = PaymentsApi("a2a4f648-a50b-42fb-bda8-00c6e2f295ea")
 
-result = pay_api.Merchant.get_merchants()
+result = payments_api.merchant.get_merchants()
+
 
 if result.is_success():
     print(result.body)
