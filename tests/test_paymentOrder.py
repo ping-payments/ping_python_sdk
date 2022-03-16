7652ffa3e1d4104821657b50585b2b476e4e1e8b
LJsson <ludvig.johansson@live.se>
2022-03-16 16:21:51 +0100

    made another test

--- tests/test_paymentOrder.py
+++ tests/test_paymentOrder.py
@@ -1,14 +1,26 @@
+
 from ping.payments_api import PaymentsApi
 import unittest
 
-class TestMerchant(unittest.TestCase):
+class TestPaymentOrder(unittest.TestCase):
     
     @classmethod
     def setUpClass(cls):
         cls.payments_api = PaymentsApi("a2a4f648-a50b-42fb-bda8-00c6e2f295ea")
 
     def test_get(self):
-        pass
+        
+        date_from = "2019-10-12"
+        date_to = "2020-10-12"
+
+        self.assertEqual(
+            self.payments_api.paymentOrder.get_payments_orders(date_from, date_to).status_code, 
+            200
+            )
+        self.assertEqual(
+            self.payments_api.paymentOrder.get_payments_orders().status_code, 
+            200
+            )
 
     def test_create(self):
         pass
