f5436ae2ec068be47a5cfa55504c00c8a5d66ba5
LJsson <ludvig.johansson@live.se>
2022-03-18 12:13:27 +0100

    more tests in pay and merch

--- tests/test_payment.py
+++ tests/test_payment.py
@@ -1,60 +1,82 @@
 from ping.payments_api import PaymentsApi
 import unittest
+from test_helper import testHelper
+import uuid
 
 
 class TestPayment(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
-        tenant_id = "a2a4f648-a50b-42fb-bda8-00c6e2f295ea"
-        cls.payments_api = PaymentsApi(tenant_id)
-
-    def test_get_payment(self):
-        payment_order_id = "8a6a4586-e082-49fe-b408-b92e9dc746ec"
-        payment_id = "c498dba8-bf28-4252-a16a-7c6192c05bc9"
-
-        response = self.payments_api.payment.get_payment(payment_order_id, payment_id)
-        self.run_tests(response)
-
-    def test_initiate_payment(self):
-
-        payment_order_id = "8a6a4586-e082-49fe-b408-b92e9dc746ec"
-        dummy_body = {
-             "currency": "SEK",
-             "merchant_amounts": {
-                 "075b5c3c-3f17-435d-ab84-0bc57d8e67d4": 9400,
-                 },
-             "metadata": {
-                 "delivery_id": "368745"
-                 },
-             "method": "dummy",
-             "order_items": [
-                 {
-                     "amount": 2500,
-                     "name": "Delivery, Marios Pasta (Pasta La Vista)",
-                     "vat_rate": 12
-                 },
-                 {
-                     "amount": 6900,
-                     "name": "Marios Pasta (Pasta La Vista)",
-                     "vat_rate": 12
-                 }
-             ],
-             "provider": "dummy",
-             "provider_method_parameters": {},
-             "status_callback_url": "https://somesite.com/callback"
+        cls.test_helper = testHelper
+        cls.payment_order_id = "8a6a4586-e082-49fe-b408-b92e9dc746ec"
+        cls.payments_api = PaymentsApi(
+            tenant_id = "a2a4f648-a50b-42fb-bda8-00c6e2f295ea"
+        )
+        
+        
+    def setUp(self):
+        self.dummy_body = {
+            "currency": "SEK",
+            "merchant_amounts": {
+                "075b5c3c-3f17-435d-ab84-0bc57d8e67d4": 9400,
+                },
+            "metadata": {
+                "delivery_id": "368745"
+                },
+            "method": "dummy",
+            "order_items": [
+                {
+                    "amount": 2500,
+                    "name": "Delivery, Marios Pasta (Pasta La Vista)",
+                    "vat_rate": 12
+                },
+                {
+                    "amount": 6900,
+                    "name": "Marios Pasta (Pasta La Vista)",
+                    "vat_rate": 12
+                }
+            ],
+            "provider": "dummy",
+            "provider_method_parameters": {},
+            "status_callback_url": "https://somesite.com/callback"
         }
 
-        response = self.payments_api.payment.initiate_payment(dummy_body, payment_order_id)
-        self.run_tests(response)
+# Get Payments Tests
+    # gets payment correctly
+    def test_get_payment_200(self):
+        payment_id = "c498dba8-bf28-4252-a16a-7c6192c05bc9"
 
-    def run_tests(self, response):
-        self.assertIsNotNone(response)
-        self.assertEqual(response.status_code, 200)
-        self.assertFalse(response.is_error())
-        self.assertTrue(response.is_success())
-        self.assertIsNotNone(response.body)
-        self.assertIsNone(response.errors)
+        response = self.payments_api.payment.get_payment(self.payment_order_id, payment_id)
+        self.test_helper.run_tests(self, response)
+    
+    # gets payment with an incorrect id
+    def test_get_payment_404(self):
+        payment_id = ""
+
+        response = self.payments_api.payment.get_payment(self.payment_order_id, payment_id)
+        self.test_helper.run_tests(self, response, 404)
+
+# Initiate Payment Tests
+    # Initiate a correct payment (status code 200)
+    def test_initiate_payment_200(self):
+        response = self.payments_api.payment.initiate_payment(self.dummy_body, self.payment_order_id)
+        self.test_helper.run_tests(self, response)
+
+    # Initiate a payment with incorrect values inside payment object (status code 422)
+    def test_initiate_payment_422(self):
+        payment_order_id = "8a6a4586-e082-49fe-b408-b92e9dc746ec"
+        self.dummy_body["method"] = 0
+
+        response = self.payments_api.payment.initiate_payment(self.dummy_body, payment_order_id)
+        self.test_helper.run_tests(self, response, 422)
+
+    # Initiate a payment on a non-exisiting payment order (status code 404)
+    def test_initiate_payment_404(self):
+        error_payment_order_id = ""
+
+        response = self.payments_api.payment.initiate_payment(self.dummy_body, error_payment_order_id)
+        self.test_helper.run_tests(self, response, 404)
 
 
 if __name__ == '__main__':
--- tests/test_paymentOrder.py
+++ tests/test_paymentOrder.py
@@ -1,6 +1,6 @@
 from ping.payments_api import PaymentsApi
 import unittest
-
+from test_helper import testHelper
 
 class TestPaymentOrder(unittest.TestCase):
 
@@ -9,6 +9,7 @@ class TestPaymentOrder(unittest.TestCase):
         cls.payments_api = PaymentsApi("a2a4f648-a50b-42fb-bda8-00c6e2f295ea")
         cls.payment_order_id = "bd3e750f-2213-45c5-9d02-0dbeb2178675"
         cls.split_tree_id = "4f3a07d4-ef83-4040-bcc4-0a6e6bfab6ab"
+        cls.test_helper = testHelper
 
     def test_get_payment_orders(self):
         response_date = self.payments_api.paymentOrder.get_payments_orders("2019-10-12", "2020-10-12")
