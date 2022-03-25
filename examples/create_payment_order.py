c0c4cbf6570e85831e936997eb2f725ddd24a76d
Gustav Fransson <gustavfransson.extra@gmail.com>
2022-03-25 15:47:31 +0100

    fixed create_paymnet_order

--- examples/create_payment_order.py
+++ examples/create_payment_order.py
@@ -4,8 +4,7 @@ tenant_id = "a2a4f648-a50b-42fb-bda8-00c6e2f295ea"
 payments_api = PaymentsApi(tenant_id)
 
 split_tree_id = "4f3a07d4-ef83-4040-bcc4-0a6e6bfab6ab"
-payment_order_id = "4de6d9f0-5152-443d-8472-f70278a4190b"
-result = payments_api.paymentOrder.update_payment_order(payment_order_id, split_tree_id)
+result = payments_api.paymentOrder.create_payment_order(split_tree_id)
 
 if result.is_success():
  print(result.body)
