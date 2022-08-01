3c601a59cba26d71433243c67e0b44d5790b0f4b
LJsson <ludvig.johansson@live.se>
2022-08-01 09:05:35 +0200

    example revert

--- examples/get_merchants.py
+++ examples/get_merchants.py
@@ -5,8 +5,15 @@ from ping.payments_api import PaymentsApi
 
 
 load_dotenv()
-tenant_id = os.getenv("TENANT_ID")
-payment_links_api = PaymentsApi(tenant_id)
 
-response = payment_links_api.merchant.list()
-print(response)
+tenant_id = os.getenv("TENANT_ID")
+payments_api = PaymentsApi(tenant_id)
+
+result = payments_api.merchant.list()
+if result.is_success():
+ print(result.body)
+ print("success")
+elif result.is_error():
+ print(result.errors)
+ print("error")
+
